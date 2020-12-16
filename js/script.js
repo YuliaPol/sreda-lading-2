d55885dbb0a5dccb905c8df8622a82866b94875f
YuliaPol <julia.polyeshko@gmail.com>
2020-12-16 17:03:53 +0200

    add reviews section

--- js/script.js
+++ js/script.js
@@ -34,5 +34,31 @@ jQuery(function ($) {
                 }
             ],
         });
+        $('.screenshots_slider').slick({
+          infinite: true,
+          dots: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          responsive: [
+              {
+                  breakpoint: 1920,
+                  settings: {
+                    slidesToShow: 3,
+                  }
+              },
+              {
+                breakpoint: 993,
+                settings: {
+                  slidesToShow: 2,
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 1,
+                }
+              }
+          ],
+      });
     });
 });
